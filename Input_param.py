33ffc089a74f7e03213b3f3ef86cbf09722b5222
yuxiwang_laptop <yusnwang232@gmail.com>
2024-09-01 11:13:43 -0700

    1.7 9/1/2024 Arelion optimize

--- Input_param.py
+++ Input_param.py
@@ -3,11 +3,11 @@ from datetime import datetime
 
 def input_param():
     # Set the filter start time, end time, folder_name, account_name
-    start = input("Enter the filter start time like: 2024,8,29,10,2 (No ahead zero like 08)\n")
+    start = input("Enter the filter start time like: 2024,8,29,10,2\n")
     start = start.split(",")
     start_date = datetime(int(start[0]), int(start[1]), int(start[2]), int(start[3]), int(start[4]))
     print(f"Your start time: {start_date}")
-    end = input("Enter the filter end time like: 2024,8,29,10,2 (No ahead zero like 08)\n")
+    end = input("Enter the filter end time like: 2024,8,29,10,2\n")
     end = end.split(",")
     end_date = datetime(int(end[0]), int(end[1]), int(end[2]), int(end[3]), int(end[4]))
     print(f"Your end time: {end_date}")
--- Sender_filter.py
+++ Sender_filter.py
@@ -3,7 +3,7 @@ def sender_filter():
     filter_list = ['csc', 'Equinix Maintenance', 'RCP.Frankfurt@telekom.de', 'GCX Change Management',
                    'Flexential Support', 'Planned Work/Maintenance <infraco.cm@exainfra.net>',
                    "Digital Realty Customer Portal <customerportal@digitalrealty.com>"]
-    sender_list = ['No-Reply@Lumen.com', 'MR Zayo', 'americas-csc@verizonbusiness.com']
+    sender_list = ['No-Reply@Lumen.com', 'MR Zayo', 'americas-csc@verizonbusiness.com', 'ncm@arelion.com']
     # sender_list = ['americas-csc@verizonbusiness.com']
     subject_list = []
     return sender_list, subject_list
